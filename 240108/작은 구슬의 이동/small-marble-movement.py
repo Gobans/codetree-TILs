3c9720b62d5bace626aa5cd2fcf7399e50e98921
Goban <gobanest@gmail.com>
2024-01-08 20:38:12 +0900

    Title: 작은 구슬의 이동, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240108/작은 구슬의 이동/small-marble-movement.py
+++ 240108/작은 구슬의 이동/small-marble-movement.py
@@ -13,14 +13,15 @@ dxs, dys = [0, 1, -1, 0], [1, 0, 0, -1]
 dir = mapper[d]
 def in_range(x, y):
     return x >= 1 and x <= n and y >= 1 and y <= n
-nowt = 0
+nowt = 1
 while nowt <= t:
+    # 이동
     r += dys[dir]
     c += dxs[dir]
+    # 방향전환
     if in_range(r, c) == False:
         dir = 3 - dir
         r += dys[dir]
         c += dxs[dir]
-        nowt += 1
     nowt += 1
 print(r, c)
