283927a87c9f5b3c9c3d7f46b1309a0bfa876a56
Goban <gobanest@gmail.com>
2024-01-09 19:59:44 +0900

    Title: 2개 이상의 알파벳, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240109/2개 이상의 알파벳/more-than-one-alphabet.py
+++ 240109/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -1,7 +1,8 @@
 A = input()
 def is_more_c(A):
-    for i in range(len(A)-1):
-        if A[i] in A[i+1:]:
+    f = A[0]
+    for i in range(1, len(A)):
+        if A[i] != f:
             return True
     return False
 if is_more_c(A):
