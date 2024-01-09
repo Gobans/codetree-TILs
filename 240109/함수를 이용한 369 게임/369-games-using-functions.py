aeb89c920270cebc99d77314e05d96408b1c3b51
Goban <gobanest@gmail.com>
2024-01-09 13:16:18 +0900

    Title: 함수를 이용한 369 게임, Time: 227ms, Memory: 25MB, Status: Passed - CodeTree

--- 240109/함수를 이용한 369 게임/369-games-using-functions.py
+++ 240109/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,8 +1,9 @@
 def is_magic_number(n):
     strN = str(n)
-    if '3' in strN or '6' in strN or '9' in strN:
-        return True
-    elif n%3 == 0:
+    for digit in ['3', '6', '9']:
+        if digit in strN:
+            return True
+    if n%3 == 0:
         return True
     return False
 def count_num(a, b):
