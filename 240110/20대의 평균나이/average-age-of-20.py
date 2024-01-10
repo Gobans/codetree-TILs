b0dd9feec7c88e9da59189164404b7a89f8aac20
Goban <gobanest@gmail.com>
2024-01-10 11:40:49 +0900

    Title: 20대의 평균나이, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240110/20대의 평균나이/average-age-of-20.py
+++ 240110/20대의 평균나이/average-age-of-20.py
@@ -5,6 +5,7 @@ while True:
         if n >= 30:
             answer = sum(l) / len(l)
             print("{:.2f}".format(round(answer, 2)))
+            break
         l.append(n)
     except EOFError:
         break
