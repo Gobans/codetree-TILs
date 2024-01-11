2db7ecaff410216678952506b57ec4b8c0155d27
Goban <gobanest@gmail.com>
2024-01-11 22:49:40 +0900

    Title: 규칙에 따라 밀기, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240111/규칙에 따라 밀기/push-by-the-rules.py
+++ 240111/규칙에 따라 밀기/push-by-the-rules.py
@@ -3,15 +3,8 @@ command = input()
 l, r = 0, 0
 for c in command:
     if c == "L":
-        if r > 0:
-            r -= 1
-        else:
-            l += 1
+        l += 1
     else:
-        if l > 0:
-            l -= 1
-        else:
-            r += 1
+        r += 1
 s = s[l:] + s[:-r] if r > 0 else s[l:]
-
 print(s)
