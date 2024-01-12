3f95fafaf5a57f8fa7e7f45a57f4483d17e0d6ea
Goban <gobanest@gmail.com>
2024-01-12 18:45:56 +0900

    Title: 그 요일은, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240112/그 요일은/the-day-of-the-day.py
+++ 240112/그 요일은/the-day-of-the-day.py
@@ -1,19 +1,40 @@
 m1, d1, m2, d2 = map(int, input().split())
 remainDays = 0
-days = [31,29,31,30,31,30,31,31,30,31,30,31]
-dof = input()
-for m in range(m1, m2+1):
-    r = 0
-    if m == m1:
-        r = days[m1-1] - d1
-    elif m == m2:
-        r = d2
-    else:
-        r = days[m]
-    remainDays += r
-dofs = ["", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-answer = 0
-answer += remainDays//7
-if dof in dofs[:remainDays%7+1]:
-    answer += 1
-print(answer)
+target_day = input()
+def days_in_month(month, year):
+    if month in {1, 3, 5, 7, 8, 10, 12}:
+        return 31
+    elif month in {4, 6, 9, 11}:
+        return 30
+    elif month == 2:
+        return 29
+
+def count_days_between_dates(m1, d1, m2, d2, target_day):
+    days_in_month1 = days_in_month(m1, 2024)
+    days_in_month2 = days_in_month(m2, 2024)
+
+    current_month = m1
+    current_day = d1
+    target_day_index = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"].index(target_day)
+
+    count = 0
+
+    while current_month != m2 or current_day != d2:
+        if current_day == 1:
+            target_day_index = (target_day_index + 1) % 7
+
+        if target_day_index == (current_day - 1) % 7:
+            count += 1
+
+        current_day += 1
+
+        if current_day > days_in_month1:
+            current_day = 1
+            current_month += 1
+
+        days_in_month1 = days_in_month(current_month, 2024)
+
+    return count
+
+result = count_days_between_dates(m1, d1, m2, d2, target_day)
+print(result)
