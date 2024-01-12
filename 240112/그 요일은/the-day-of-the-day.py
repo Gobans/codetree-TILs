7232943dbe5b0375b12d8d9061d9d3e16a410482
Goban <gobanest@gmail.com>
2024-01-12 18:31:08 +0900

    Title: 그 요일은, Time: 133ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240112/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,19 @@
+m1, d1, m2, d2 = map(int, input().split())
+remainDays = 0
+days = [31,29,31,30,31,30,31,31,30,31,30,31]
+dof = input()
+for m in range(m1, m2+1):
+    r = 0
+    if m == m1:
+        r = days[m1-1] - d1
+    elif m == m2:
+        r = d2
+    else:
+        r = days[m]
+    remainDays += r
+dofs = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+answer = 0
+answer += remainDays//7
+if dof in dofs[:remainDays%7+1]:
+    answer += 1
+print(answer)
