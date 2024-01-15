1696481d389a04e3be55e9d2f10a5ac6e384b685
Goban <gobanest@gmail.com>
2024-01-15 19:41:20 +0900

    Title: 최고의 13위치, Time: 180ms, Memory: 24MB, Status: Passed - CodeTree

--- 240115/최고의 13위치/best-place-of-13.py
+++ 240115/최고의 13위치/best-place-of-13.py
@@ -5,8 +5,7 @@ for _ in range(N):
     board.append(l)
 max_cnt = 0
 for i in range(N):
-    cnt = 0
     for j in range(N-2):
         cnt = board[i][j] + board[i][j+1] + board[i][j+2]
-    max_cnt = max(max_cnt, cnt)
+        max_cnt = max(max_cnt, cnt)
 print(max_cnt)
