722041d818ef1619b324c2285baa4bbd5e30432d
Goban <gobanest@gmail.com>
2024-02-02 13:30:31 +0900

    Title: X 달리기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240202/X 달리기/x-run.py
@@ -0,0 +1,22 @@
+import sys
+x = int(input())
+min_time = sys.maxsize
+
+def running(distance, speed, time):
+    global min_time
+    if distance > x or min_time <= time:
+        return
+    if distance == x and speed == 1:
+        min_time = min(min_time, time)
+        return
+    elif distance == x and speed > 1:
+        return
+    
+    now_distance = distance + speed
+    running(now_distance, speed, time + 1)
+    running(now_distance, speed + 1, time + 1)
+    if speed > 1:
+        running(now_distance, speed -1, time + 1)
+
+running(0, 1, 1)
+print(min_time)
