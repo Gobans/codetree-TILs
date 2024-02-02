49afbe974ecba9b03311953e24839a86999a4bed
Goban <gobanest@gmail.com>
2024-02-02 18:30:29 +0900

    Title: 코딩톡, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240202/코딩톡/coding-talk.py
@@ -0,0 +1,23 @@
+import sys
+
+n, m, p = tuple(map(int, input().split()))
+message = [
+    list(input().split())
+    for _ in range(m)
+]
+
+if int(message[p - 1][1]) == 0:
+    sys.exit()
+
+for i in range(n):
+
+    person = chr(ord('A') + i)
+    read = False
+
+    for c, u in message:
+        u = int(u)
+        if u >= int(message[p - 1][1]) and c == person:
+            read = True
+    
+    if read == False:
+        print(person, end=" ")
