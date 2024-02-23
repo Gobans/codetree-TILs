a58d8afe8f5d908da28b76e627947944baaeb166
Goban <gobanest@gmail.com>
2024-02-23 14:41:54 +0900

    Title: 개발자의 가위바위보, Time: 131ms, Memory: 25MB, Status: Passed - CodeTree

--- 240223/개발자의 가위바위보/developer's-rock-paper-scissors.py
+++ 240223/개발자의 가위바위보/developer's-rock-paper-scissors.py
@@ -4,14 +4,14 @@ N = int(input())
 dict = {}
 for i in range(N):
     a, b = map(int, input().split())
-    for i in range(1,4):
+    for i in range(1, 4):
         rock = i
         for j in range(1, 4):
             if j == rock:
                 continue
             scissor = j
             for k in range(1, 4):
-                if k == scissor:
+                if k == scissor or k == rock:
                     continue
                 papaer = k
                 if (a == rock and b == scissor) or (a == scissor and b == papaer) or (a == papaer and b == rock):
