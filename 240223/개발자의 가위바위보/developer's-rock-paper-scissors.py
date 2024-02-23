c27d6c9b7a623a28093204c3e2c18eb2adadcdb3
Goban <gobanest@gmail.com>
2024-02-23 14:39:55 +0900

    Title: 개발자의 가위바위보, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240223/개발자의 가위바위보/developer's-rock-paper-scissors.py
@@ -0,0 +1,24 @@
+# 가위바위보 셋트를 저장, 각각 가정을 
+
+N = int(input())
+dict = {}
+for i in range(N):
+    a, b = map(int, input().split())
+    for i in range(3):
+        rock = i
+        for j in range(3):
+            if j == rock:
+                continue
+            scissor = j
+            for k in range(3):
+                if k == scissor:
+                    continue
+                papaer = k
+                if (a == rock and b == scissor) or (a == scissor and b == papaer) or (a == papaer and b == rock):
+                    key = (rock, scissor, papaer)
+                    if key in dict:
+                        dict[key] += 1
+                    else:
+                        dict[key] = 1
+
+print(max(dict.values()))
