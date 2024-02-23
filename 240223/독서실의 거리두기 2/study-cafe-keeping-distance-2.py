1a39fc118e9fec05341c0f77a9090de8c559b65e
Goban <gobanest@gmail.com>
2024-02-23 15:39:04 +0900

    Title: 독서실의 거리두기 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240223/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -0,0 +1,41 @@
+N = int(input())
+line = list(map(int, input()))
+
+def find_farthest_distance_coordinate():
+    farthest_distance = 0
+    tempX = 1
+    x, y = 0, 1
+    cnt = 0
+    for i in range(len(line)):
+        if line[i] == 1 or i == len(line) - 1:
+            farthest_distance = max(farthest_distance, cnt)
+            if farthest_distance == cnt:
+                x = tempX
+                y = i
+            tempX = i
+            cnt = 0
+        else:
+            cnt += 1
+    return (x, y)
+
+def find_nearest_distance():
+    nearest_distance = len(line)
+    cnt = 0
+    for i in range(len(line)):
+        if (line[i] == 1 or i == len(line) - 1) and i != 0:
+            if i == len(line) - 1 and line[i] == 0:
+                cnt += 1
+            nearest_distance = min(nearest_distance, cnt)
+            cnt = 0
+        else:
+            cnt += 1
+    return nearest_distance + 1
+
+x, y = find_farthest_distance_coordinate()
+if y == len(line) -1:
+    line[y] = 1
+elif x == 0 and line[0] == 0:
+    line[0] = 1
+else:
+    line[(x+y)//2] = 1
+print(find_nearest_distance())
